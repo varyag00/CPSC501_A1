6bc83c83a9e7718c24f4849869505aab521ab9fe
Dan Gonzalez <d.gonzalez0902@gmail.com>
2015-10-05 02:31:06 -0600

    Refactoring 3: Extract Class

---
+++ BartenderRefactored/src/Bartender.java
@@ -0,0 +1,97 @@
+import java.util.Map;
+import java.util.Scanner;
+
+public class Bartender {
+	
+	private Scanner in;
+	private int age;
+	private Map<String, Boolean> drinks;
+	
+	public Bartender(Scanner in, int age, Map<String, Boolean> drinks){
+		
+		this.in = in;
+		this.age = age;
+		this.drinks = drinks;
+	}
+	
+	public void Run()
+	{				
+		String input;
+		
+		while (true) {
+			input = in.next();
+	        
+			if (input.contains("list")){
+				System.out.println("Commands: \n\n order <String drink> \n\n teach <String drink> <bool isAlcoholic> \n\n menu");
+			}
+			
+			else if (input.contains("teach")){
+				
+					//parsing string
+				String drinkName = in.next();
+				try {
+					drinks.put(drinkName,Boolean.parseBoolean(in.next()));  
+						
+					System.out.println(drinkName + " learned!");
+				}
+				catch (Exception e){
+					System.out.println("Invalid teaching format.");
+				}
+			}
+			
+			else if (input.contains("order")){
+				
+				//check if drink exists
+				String drink = in.next();
+				boolean drinkSeen = false;
+				
+				for (Map.Entry<String, Boolean> entry : drinks.entrySet()) 
+				{ 
+					//if the bartender knows he drink
+					if (drink.equals(entry.getKey())){
+						drinkSeen = true;
+						
+						//if the drink is alcoholic, check age
+						if (Boolean.parseBoolean(entry.getValue().toString()) == true){			    		
+							if (age >= 18){
+								System.out.println("The bartender gives you a " + drink);
+							}
+							System.out.println("You're not old enough to drink " + drink);
+					    }
+						else{
+							System.out.println("The bartender gives you a " + drink);
+						}
+					} 
+					if (drinkSeen)
+						break;
+				}
+				
+				if (!drinkSeen)
+					System.out.println("The bartender doesn't know how to make " + drink);
+			}
+			
+			else if (input.contains("menu")){
+			    
+			    for (Map.Entry<String, Boolean> entry : drinks.entrySet()) 
+			    { 
+			    	System.out.print(entry.getKey());
+			        if (Boolean.parseBoolean(entry.getValue().toString()) == true){
+				        System.out.print(", Alcoholic");
+			        }
+			        System.out.println("");
+			    }
+			    
+			}
+			
+			else if (input.equals("quit")){
+				System.out.println("Bye");
+				break;
+			}
+			
+			else{
+				System.out.println("Invalid command, type \"list\" to see valid commands");
+				input = in.nextLine();
+			}
+		}	
+	}
+}
--- BartenderRefactored/src/MainTest.java
+++ BartenderRefactored/src/MainTest.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.*;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -13,10 +14,9 @@ import org.junit.Test;
 public class MainTest {
 
 	private Scanner scanner;	
-	private Map<String, Boolean> drinks;
 
 	@Test
-	public void TestGetSomeInformationFromUser(){
+	public void TestGetUserAge(){
 		String data = "12";
 		System.setIn(new ByteArrayInputStream(data.getBytes()));
 		scanner = new Scanner(System.in);
@@ -28,6 +28,23 @@ public class MainTest {
 		assertEquals(-1, Main.getUserAge(scanner));
 	}
 
-	
+//	@Test
+//	public void TestBartenderRun(){
+//	
+//		int age = 12;
+//		
+//		String data = "order Whiskey";
+//		System.setIn(new ByteArrayInputStream(data.getBytes()));
+//		scanner = new Scanner(System.in);
+//		
+//		Map<String, Boolean> drinks = new HashMap<String, Boolean>();
+//		drinks.put("AppleJuice",false);
+//		drinks.put("Beer", true);
+//		drinks.put("Whiskey", true);
+//
+//		Bartender bt = new Bartender(scanner, age, drinks);
+//		
+//		bt.Run();
+//	}
 }
 
